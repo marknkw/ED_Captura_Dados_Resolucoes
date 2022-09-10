0bb55fa0ec9f767fb97dab7549e9dbe763ad1bbc
marknkw <mark.s.g@hotmail.com>
2022-09-10 20:29:46 -0300

    N/A
    N/A

--- web_scrapping.py
+++ web_scrapping.py
@@ -6,13 +6,27 @@ import os
 from datetime import *
 #Importar time para verificar tempo de resposta de uma página
 import time
+#Importar lxml para lidar com requisição de links dentro de uma página html
+#sem realizar o download de toda a página como arquivo a ser manipulado
+from lxml import html, etree
+#Importar lxml para lidar com requisição de links dentro de uma página html
+#sem realizar o download de toda a página como arquivo a ser manipulado
+from bs4 import BeautifulSoup
+#Importar json para conversão de html para JSON
+import json
+#importar Todos os dados usando selenium, pois requests não funciona para abrir conteúdo
+#carregado com javascript
+from selenium import webdriver
+#Define path para webdriver chrome
+from webdriver_manager.chrome import ChromeDriverManager
 
 #Função que verifica se o site está acessível
 def siteAcessivel(name):
     request = requests.get(str(name))
     try:
         if request.status_code == 200:
-            print(request.status_code)
+            downloadDaPagina()
+            return lerResolucoes()
         #Verifica se o site está aceitando conexões para query com código do tipo 2
         #mas diferentes de 200
         elif request.status_code != 200 and int(request.status_code)/100 == 2:
@@ -28,11 +42,28 @@ def siteAcessivel(name):
         # O programa retornará o tipo de erro emitido pelo Site
         return "Erro: {}".format(e)
 
-#Função de query da página
-#def queryDaPesquisa():
-    #siteAcessivel(page)
+#Função de download dos links para acesso às resuluções isoladas
+def downloadDaPagina():
+    #Define o header de acesso para as requisições do siteAcessivel
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
+    }
+    #page = requests.get(lerResolucoes(), headers = headers, timeout=(5, 30))
+    #s = BeautifulSoup(page.content, 'html.parser')
+    #links = etree.HTML(str(s))
+    #print(links.xpath('//*[@id="_br_com_seatecnologia_in_buscadou_BuscaDouPortlet_hierarchy_content"]/div[1]'))
 
+    #website = html.fromstring(page.content)
+    #tree = website.xpath('//div[@id="_br_com_seatecnologia_in_buscadou_BuscaDouPortlet_hierarchy_content"]//a/@href')
+    #print(len(tree))
 
+    motorweb = webdriver.Chrome(ChromeDriverManager().install())
+    motorweb.get(lerResolucoes())
+
+    s = BeautifulSoup(motorweb.page_source, 'lxml')
+    links = s.xpath('//a/@href')
+    for i in links:
+        print(i)
 
 #Função que faz o programa esperar pelo código de requisição 200 para qualquer
 # get da página
@@ -63,8 +94,6 @@ def criarDiretorio():
     if os.path.isdir(path) == False:
         os.mkdir(path)
 
-    print(path)
-
 #Cria função para leitura de página de pesquisa das resoluções
 def lerResolucoes():
     file = open("site.txt", 'r')
@@ -77,4 +106,4 @@ def lerResolucoes():
 criarDiretorio()
 
 #Abre a página de busca das resoluções para verificar acessibilidade da página
-siteAcessivel(lerResolucoes())
+page = siteAcessivel(lerResolucoes())
